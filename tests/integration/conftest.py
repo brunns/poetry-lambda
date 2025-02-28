980dbe2d5955fea3022260b3a78c377a9506af4c
Simon Brunning <simon@brunningonline.net>
2025-02-28 06:59:50 +0000

    Move Lambda zip upload into a text fixture.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,11 @@
 import logging
+from pathlib import Path
 
 import boto3
 import botocore
 import httpx
 import pytest
+import stamina
 from botocore.client import BaseClient
 from httpx import RequestError
 from yarl import URL
@@ -41,3 +43,64 @@ def lambda_client(localstack: URL) -> BaseClient:
         endpoint_url=str(localstack),
         config=botocore.config.Config(retries={"max_attempts": 0}),
     )
+
+
+@pytest.fixture(scope="session")
+def lambda_function(lambda_client: BaseClient) -> str:
+    function_name = "lambda_function"
+    with Path("dist/poetry-lambda.zip").open("rb") as zipfile:
+        lambda_client.create_function(
+            FunctionName=function_name,
+            Runtime="python3.13",
+            Role="arn:aws:iam::123456789012:role/test-role",
+            Handler="poetry_lambda.lambda_function.lambda_handler",
+            Code={"ZipFile": zipfile.read()},
+            Timeout=180,
+            Environment={
+                "Variables": {
+                    "ETC_CHANNEL": "dummy_chat@id.007",
+                }
+            },
+        )
+    logger.info("loaded zip")
+    wait_for_function_active(function_name, lambda_client)
+    logger.info("function active")
+    return function_name
+
+
+@pytest.fixture(scope="session")
+def flask_function(lambda_client: BaseClient) -> str:
+    function_name = "flask_function"
+    with Path("dist/poetry-lambda.zip").open("rb") as zipfile:
+        lambda_client.create_function(
+            FunctionName=function_name,
+            Runtime="python3.13",
+            Role="arn:aws:iam::123456789012:role/test-role",
+            Handler="poetry_lambda.app.lambda_handler",
+            Code={"ZipFile": zipfile.read()},
+            Timeout=180,
+            Environment={
+                "Variables": {
+                    "ETC_CHANNEL": "dummy_chat@id.007",
+                }
+            },
+        )
+    logger.info("loaded zip")
+    wait_for_function_active(function_name, lambda_client)
+    logger.info("function active")
+    return function_name
+
+
+class FunctionNotActiveError(Exception):
+    """Function not yet active"""
+
+
+def wait_for_function_active(function_name, lambda_client):
+    for attempt in stamina.retry_context(on=FunctionNotActiveError):
+        with attempt:
+            logger.info("waiting")
+            response = lambda_client.get_function(FunctionName=function_name)
+            function_state = response["Configuration"]["State"]
+            logger.info("function_state %s", function_state)
+            if function_state != "Active":
+                raise FunctionNotActiveError
--- tests/integration/test_install_app.py
+++ tests/integration/test_install_app.py
@@ -1,10 +1,8 @@
 import json
 import logging
 from http import HTTPStatus
-from pathlib import Path
 
 import httpx
-import stamina
 from botocore.client import BaseClient
 from brunns.matchers.response import is_response
 from hamcrest import assert_that, contains_string, has_entries
@@ -13,30 +11,12 @@ from yarl import URL
 logger = logging.getLogger(__name__)
 
 
-def test_install_and_call_lambda(lambda_client: BaseClient):
+def test_install_and_call_lambda(lambda_client: BaseClient, lambda_function: str):
     # Given
-    function_name = "lambda_function"
-    with Path("dist/poetry-lambda.zip").open("rb") as zipfile:
-        lambda_client.create_function(
-            FunctionName=function_name,
-            Runtime="python3.13",
-            Role="arn:aws:iam::123456789012:role/test-role",
-            Handler="poetry_lambda.lambda_function.lambda_handler",
-            Code={"ZipFile": zipfile.read()},
-            Timeout=180,
-            Environment={
-                "Variables": {
-                    "ETC_CHANNEL": "dummy_chat@id.007",
-                }
-            },
-        )
-    logger.info("loaded zip")
-    wait_for_function_active(function_name, lambda_client)
-    logger.info("function active")
 
     # When
     response = lambda_client.invoke(
-        FunctionName=function_name, InvocationType="RequestResponse", Payload=json.dumps({})
+        FunctionName=lambda_function, InvocationType="RequestResponse", Payload=json.dumps({})
     )
 
     # Then
@@ -45,26 +25,8 @@ def test_install_and_call_lambda(lambda_client: BaseClient):
     assert_that(payload, has_entries(statusCode=HTTPStatus.OK, message=contains_string("Hello")))
 
 
-def test_install_and_call_lambda_flask(lambda_client: BaseClient):
+def test_install_and_call_lambda_flask(lambda_client: BaseClient, flask_function: str):
     # Given
-    function_name = "lambda_flask_function"
-    with Path("dist/poetry-lambda.zip").open("rb") as zipfile:
-        lambda_client.create_function(
-            FunctionName=function_name,
-            Runtime="python3.13",
-            Role="arn:aws:iam::123456789012:role/test-role",
-            Handler="poetry_lambda.app.lambda_handler",
-            Code={"ZipFile": zipfile.read()},
-            Timeout=180,
-            Environment={
-                "Variables": {
-                    "ETC_CHANNEL": "dummy_chat@id.007",
-                }
-            },
-        )
-    logger.info("loaded zip")
-    wait_for_function_active(function_name, lambda_client)
-    logger.info("function active")
 
     # When
     request_payload = {
@@ -78,7 +40,7 @@ def test_install_and_call_lambda_flask(lambda_client: BaseClient):
         "isBase64Encoded": False,
     }
     response = lambda_client.invoke(
-        FunctionName=function_name,
+        FunctionName=flask_function,
         InvocationType="RequestResponse",
         Payload=json.dumps(request_payload),
     )
@@ -90,29 +52,9 @@ def test_install_and_call_lambda_flask(lambda_client: BaseClient):
     assert_that(response_payload, has_entries(statusCode=HTTPStatus.OK, body=contains_string("Hello")))
 
 
-def test_install_and_call_flask_lambda_over_http(lambda_client: BaseClient):
+def test_install_and_call_flask_lambda_over_http(lambda_client: BaseClient, flask_function: str):
     # Given
-    function_name = "lambda_flask_function_for_http"
-    with Path("dist/poetry-lambda.zip").open("rb") as zipfile:
-        lambda_client.create_function(
-            FunctionName=function_name,
-            Runtime="python3.13",
-            Role="arn:aws:iam::123456789012:role/test-role",
-            Handler="poetry_lambda.app.lambda_handler",
-            Code={"ZipFile": zipfile.read()},
-            Timeout=180,
-            Environment={
-                "Variables": {
-                    "ETC_CHANNEL": "dummy_chat@id.007",
-                }
-            },
-        )
-    logger.info("loaded zip")
-    wait_for_function_active(function_name, lambda_client)
-    logger.info("function active")
-
-    response = lambda_client.create_function_url_config(FunctionName=function_name, AuthType="NONE")
-
+    response = lambda_client.create_function_url_config(FunctionName=flask_function, AuthType="NONE")
     function_url = URL(response["FunctionUrl"])
     logger.info("FunctionUrl: %s", function_url)
 
@@ -121,18 +63,3 @@ def test_install_and_call_flask_lambda_over_http(lambda_client: BaseClient):
 
     # Then
     assert_that(response, is_response().with_status_code(HTTPStatus.OK).and_body(contains_string("Hello")))
-
-
-class FunctionNotActiveError(Exception):
-    """Function not yet active"""
-
-
-def wait_for_function_active(function_name, lambda_client):
-    for attempt in stamina.retry_context(on=FunctionNotActiveError):
-        with attempt:
-            logger.info("waiting")
-            response = lambda_client.get_function(FunctionName=function_name)
-            function_state = response["Configuration"]["State"]
-            logger.info("function_state %s", function_state)
-            if function_state != "Active":
-                raise FunctionNotActiveError
